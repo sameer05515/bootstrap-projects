1d0d58ef98344484f379ad4229ef0ff863c8a913
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 03:49:04 +0530

    commit

--- Bootstrap-5-Tutorial-For-Beginners/lesson-0003-proj-02-vite-ts-approach/src/main.tsx
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0003-proj-02-vite-ts-approach/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss'
+// import './index.scss'
 import './styles/index.scss';
 import App from './App.tsx'
 
