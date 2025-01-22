08de4a11bbb486b0a5ae9ba96e2aae63a083bf6e
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 05:05:48 +0530

    commit

--- Bootstrap-5-Tutorial-For-Beginners/lesson-0003-proj-02-vite-ts-approach/src/App.tsx
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0003-proj-02-vite-ts-approach/src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from "react";
 import "./App.scss";
 import "bootstrap/scss/bootstrap.scss";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <div className="container-fluid">
-      <h1>Vite + React+ TS</h1>
-      <div className="card-body">
-        <button
-          className="btn btn-sm btn-success"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </div>
   );
 }
