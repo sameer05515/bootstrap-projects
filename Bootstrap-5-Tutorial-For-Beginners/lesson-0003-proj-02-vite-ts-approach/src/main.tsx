a61bf8374783b097fb510c8cd8d1a5fcf22a9357
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 03:43:45 +0530

    commit

--- Bootstrap-5-Tutorial-For-Beginners/lesson-0003-proj-02-vite-ts-approach/src/main.tsx
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0003-proj-02-vite-ts-approach/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss'
+import './styles/index.scss';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
