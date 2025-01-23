c4152dc26bc0afd95fc58b642c224dea52fb7ac8
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 12:43:20 +0530

    commit

---
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-004-proj-02-z-react-bootstrap-babel/comp/App.js
@@ -0,0 +1,10 @@
+const App=()=>{
+
+    return(
+        <h1>
+            Hello ! I will help to reduce redundant code. but you will have to write all jsx code just in this file only.
+        </h1>
+    )
+};
+
+export default App;
