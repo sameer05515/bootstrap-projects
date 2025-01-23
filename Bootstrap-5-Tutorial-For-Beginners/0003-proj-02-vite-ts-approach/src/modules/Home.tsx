6edd3cb605ecc298de07aac02c07eb75dcd34938
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 13:56:20 +0530

    commit

--- Bootstrap-5-Tutorial-For-Beginners/0003-proj-02-vite-ts-approach/src/modules/Home.tsx
+++ Bootstrap-5-Tutorial-For-Beginners/0003-proj-02-vite-ts-approach/src/modules/Home.tsx
@@ -1,5 +1,34 @@
+import { NavLink } from "react-router-dom";
+
+const routesArray = [
+  { title: "Home", routeUrl: "/" },
+  { title: "Apna Playgound", routeUrl: "/apna-playground" },
+].map((r, idx) => ({
+  ...r,
+  id: `route-${idx + 1}`,
+}));
+
 const Home = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="container-fluid">
+      <div
+        style={{
+          display: "block",
+          justifyItems: "center",
+          fontWeight: "bold",
+          paddingTop: "4px",
+          paddingBottom: "4px",
+          width: "100%",
+        }}
+      >
+        {routesArray.map(({ id, title, routeUrl }) => (
+          <NavLink key={id} to={routeUrl} style={{ textAlign: "center" }}>
+            <div>{title}</div>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
