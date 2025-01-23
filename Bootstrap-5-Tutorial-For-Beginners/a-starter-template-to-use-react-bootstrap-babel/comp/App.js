17123824c31d046c66bd411506d8c50b8f99146d
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 12:49:11 +0530

    commit

--- Bootstrap-5-Tutorial-For-Beginners/a-starter-template-to-use-react-bootstrap-babel/comp/App.js
+++ Bootstrap-5-Tutorial-For-Beginners/a-starter-template-to-use-react-bootstrap-babel/comp/App.js
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <h1>
       Hello ! I will help to reduce redundant code. but you will have to write
-      all jsx code just in this file only.
+      all jsx code just in this file (i.e. App.js) only.
     </h1>
   );
 };
