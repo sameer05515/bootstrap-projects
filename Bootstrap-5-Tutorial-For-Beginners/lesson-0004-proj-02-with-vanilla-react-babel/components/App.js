dd7cf633e752a12bc1e271cfaa274d70b5fdf2c1
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 08:37:36 +0530

    commit

---
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0004-proj-02-with-vanilla-react-babel/components/App.js
@@ -0,0 +1,15 @@
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+const App = () => {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(Header),
+    React.createElement('p', null, 'Welcome to React with Vanilla JavaScript, using standalone babel!'),
+    React.createElement(Footer)
+  );
+};
+
+// Render the App component to the DOM
+ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(App));
