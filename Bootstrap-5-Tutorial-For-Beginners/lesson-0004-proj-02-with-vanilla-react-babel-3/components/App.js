0faf6b9b519ed771a924b63d296575bc5b04eda2
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 09:06:55 +0530

    commit

---
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0004-proj-02-with-vanilla-react-babel-3/components/App.js
@@ -0,0 +1,17 @@
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to React with Vanilla JavaScript, using standalone babel and JSX syntax!</p>
+      <Footer />
+    </div>
+  );
+};
+
+// Render the App component to the DOM
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+export default App;
---
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0004-proj-02-with-vanilla-react-babel-3/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>This is the Footer</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
