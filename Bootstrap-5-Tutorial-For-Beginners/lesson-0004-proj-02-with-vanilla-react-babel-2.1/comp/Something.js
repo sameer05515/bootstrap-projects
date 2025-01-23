cef34cf84d55d16960b42945e69e6ea5dc7be0f3
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-23 12:07:10 +0530

    commit

--- Bootstrap-5-Tutorial-For-Beginners/lesson-0004-proj-02-with-vanilla-react-babel-2.1/comp/Something.js
+++ Bootstrap-5-Tutorial-For-Beginners/lesson-0004-proj-02-with-vanilla-react-babel-2.1/comp/Something.js
@@ -6,11 +6,14 @@ const SomethingElse = () => {
   )
 }
 
-export const Something = () => {
+const Something = () => {
   return (
     <>
       <h1>Hello from Something2 Component from comp folder!</h1>
+      <h2>Babel stanalone can only use single jsx file. It does not resolve depending jsx files of main jsx file.</h2>
       <SomethingElse />
     </>
   );
 };
+
+export default Something;
